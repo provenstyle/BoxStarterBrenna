c10918d5ff506b374a5b6f9a485f9c552356879a
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-10-13 21:22:03 -0500

    more apps

--- BoxStarterBrenna.ps1
+++ BoxStarterBrenna.ps1
@@ -1,6 +1,10 @@
 Set-WindowsExplorerOptions -EnableShowFileExtensions
 
-choco install GoogleChrome
-choco install 
-choco install iTunes
+choco install GoogleChrome 
 choco install FoxitReader
+choco install VirtualCloneDrive
+choco install iTunes
+choco install spotify
+choco install silverlight
+choco install you-need-a-budget
+choco install dropbox
